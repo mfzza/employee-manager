65f47e383cb5c72f852ceab115e0f8835f4c023d
fatkhulizz <fatkhul7izza@gmail.com>
2025-10-22 15:25:18 +0700

    Continue implement List options for cli - better view using table library - added shortcut to select sorting - etc

--- internal/cli/main_menu.go
+++ internal/cli/main_menu.go
@@ -37,37 +37,63 @@ func InitialModel(s *employee.Service) Model {
 func (m Model) Init() tea.Cmd {
 	return nil
 }
-
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	// NOTE: triple nested switch!!! maybe split it into another file
+	// for checking what kinda of input
 	switch msg := msg.(type) {
-
 	case tea.KeyMsg:
-		switch msg.String() {
+		// check what state we are in
+		switch m.state {
+		case optList:
+			var cmd tea.Cmd
+			m.table, cmd = m.table.Update(msg)
 
-		case strconv.Itoa(int(optAdd)):
-			m.message = "You chose: Add"
-		case strconv.Itoa(int(optList)):
-			m.message = "You chose: List"
-			m.state = optList
-		case strconv.Itoa(int(optView)):
-			m.message = "You chose: View"
-		case strconv.Itoa(int(optEdit)):
-			m.message = "You chose: Edit"
-		case strconv.Itoa(int(optDelete)):
-			m.message = "You chose: Delete"
+			// key shortcut
+			// TODO: add hints
+			switch msg.String() {
+			case "m", "M":
+				m.state = optMenu
+				return m, nil
+			case "i", "I":
+				m.table = m.createTable("id")
+				return m, nil
+			case "n", "N":
+				m.table = m.createTable("name")
+				return m, nil
+			case "p", "P":
+				m.table = m.createTable("phone")
+				return m, nil
+			case "q", "Q", "ctrl+c":
+				m.quitting = true
+				return m, tea.Quit
+			}
+			return m, cmd
 
-		case "q", "Q", "ctrl+c":
-			m.message = "Exiting program..."
-			m.quitting = true
-			return m, tea.Quit
-
-		case "m", "M":
-			m.state = optMenu
-
-		default:
-			m.message = "Invalid option. Press 0-6."
+		case optMenu:
+			switch msg.String() {
+			case strconv.Itoa(int(optAdd)):
+				m.message = "You chose: Add"
+			case strconv.Itoa(int(optList)):
+				m.message = "You chose: List"
+				m.state = optList
+				// init table when user select: List
+				m.table = m.createTable("id")
+			case strconv.Itoa(int(optView)):
+				m.message = "You chose: View"
+			case strconv.Itoa(int(optEdit)):
+				m.message = "You chose: Edit"
+			case strconv.Itoa(int(optDelete)):
+				m.message = "You chose: Delete"
+			case "q", "Q", "ctrl+c":
+				m.message = "Exiting program..."
+				m.quitting = true
+				return m, tea.Quit
+			default:
+				m.message = "Invalid option. Press 0-6."
+			}
 		}
 	}
+
 	return m, nil
 }
 
@@ -83,7 +109,7 @@ func (m Model) View() string {
 
 	case optList:
 		render += renderHeader("LIST")
-		render += m.createTable().View() + "\n"
+		render += m.table.View() + "\n"
 	}
 	return render
 }
@@ -122,14 +148,14 @@ Q. Quit
 `, optAdd, optList, optView, optEdit, optDelete)
 }
 
-func (m Model) createTable() table.Model {
+func (m Model) createTable(sorting string) table.Model {
 	columns := []table.Column{
 		{Title: "ID", Width: 3},
 		{Title: "Name", Width: 20},
 		{Title: "Phone", Width: 20},
 	}
 
-	employees := m.service.List("id")
+	employees := m.service.List(sorting)
 	// Convert to []table.Row
 	var rows []table.Row
 	for _, emp := range employees {
